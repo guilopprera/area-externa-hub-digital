d9cd8c4c92b6ea6601e36ace15837cd580f9919b
guilopprera <gui-lopespereira@hotmail.com>
2023-04-24 10:31:09 -0300

    atualização imagens jenifer

--- src/app/pages/quem-somos/quem-somos.component.ts
+++ src/app/pages/quem-somos/quem-somos.component.ts
@@ -21,7 +21,7 @@ export class QuemSomosComponent {
   getListMedias() {
     this.listMedias.push({ id: 1, title: "Entenda como evitar o teatro da inovação corporativa", text: "Vivemos uma realidade em que algumas empresas estão mais preocupadas ..." })
     this.listMedias.push({ id: 2, title: "Fintechs: o que são e como essas empresas impactam o mercado?", text: "Com um sistema financeiro historicamenteengessado e burocrático, o B ..." })
-    this.listMedias.push({ id: 3, title: "Qual é o panorama atual dasfintechs no Brasil?", text: "A crise econômica causada pela pandemia decovid-19 não abalou o set ..." })
+    this.listMedias.push({ id: 3, title: "Qual é o panorama atual dasfintechs no Brasil?", text: "A crise econômica causada pela pandemia decovid-19 não abalou o set ..."})
   }
 
   teste() {
